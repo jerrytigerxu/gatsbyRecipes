e9e57e0871c8823ff5b87cf2ded6fa4c94dac0c8
jerrytigerxu <jere.tiger.xu@gmailc.om>
2023-03-24 17:49:05 -0400

    Cleaning up

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,24 +1,22 @@
 import React from 'react'
 import Layout from '../components/Layout'
 
-import * as aboutStyles from '../examples/about.module.css'
 import styled from 'styled-components'
-
 
 const About = () => {
   return (
     <Layout>
-      <h2 className={aboutStyles.page}>This is the about page!</h2>
+      <Wrapper>
+        <h2 className="text">This is the about page!</h2>
+      </Wrapper>
     </Layout>
   )
 }
 
 const Wrapper = styled.section`
-  .page {
-    color: red;
-  }
+  color: red;
 
-  .page h1 {
+  h1 {
     color: yellowgreen;
   }
 
